58778d5570cb93778f4bbbf447b41019a71ba471
Tiago Azevedo <56261720+tiagoazeved0@users.noreply.github.com>
2022-10-10 16:19:13 +0100

    Updates to Feature 1

--- cypress/support/step_definitions/stepDefinitions.js
+++ cypress/support/step_definitions/stepDefinitions.js
@@ -6,20 +6,25 @@ const {
 
 Given("that I open the browser with the url {string}", function (string) {
   cy.visit(string);
-
-  cy.get('h1').contains('UI Test Automation');
+  cy.get("h1").contains("UI Test Automation");
 });
 
 When("we press the UI Playground Option {string}", function (string) {
   cy.get("a:contains('" + string + "')").click();
-
-  cy.get('h3').contains(string);
+  cy.get("h3").contains(string);
 });
 
 Then("we click the button with the Dynamic ID with success", function () {
-    cy.get("button:contains('Button with Dynamic ID')").click();
+  cy.get("button:contains('Button with Dynamic ID')")
+    .should("have.text", "Button with Dynamic ID")
+    .click();
 });
 
-Given("we click the button with {string} in {string} attribute", function (string, string2) {
-  cy.get("#" + string).should('have.id', string).click();
-  }); 
+Given(
+  "we click the button with {string} in {string} attribute",
+  function (string, string2) {
+    cy.get("#" + string)
+      .should("have.id", string)
+      .click();
+  }
+);
