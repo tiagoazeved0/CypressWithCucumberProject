27f090e8f989147c15564e90fa6de8619fa53cb1
Tiago Azevedo <56261720+tiagoazeved0@users.noreply.github.com>
2022-10-10 16:14:16 +0100

    Feature 2 Added

---
+++ cypress/UIAutomationPlayground/HiddenLayers.feature
@@ -0,0 +1,15 @@
+
+@UIFeatures
+Feature: UI Testing Playground - Feature 3 - HiddenLayers
+
+  @Feature3
+  Scenario Outline: Clicking the Green Button and then the Blue Button
+    Given that I open the browser with the url <url>
+    When we press the UI Playground Option <option>
+    And we click the button with "greenButton" in "id" attribute
+    And we click the button with "blueButton" in "id" attribute
+
+
+    Examples:
+      | url                                       | option       |
+      | "http://www.uitestingplayground.com/home" | "Hidden Layers" |
--- cypress/support/step_definitions/stepDefinitions.js
+++ cypress/support/step_definitions/stepDefinitions.js
@@ -13,9 +13,13 @@ Given("that I open the browser with the url {string}", function (string) {
 When("we press the UI Playground Option {string}", function (string) {
   cy.get("a:contains('" + string + "')").click();
 
-  cy.get('button').contains('Button with Dynamic ID');
+  cy.get('h3').contains(string);
 });
 
 Then("we click the button with the Dynamic ID with success", function () {
     cy.get("button:contains('Button with Dynamic ID')").click();
 });
+
+Given("we click the button with {string} in {string} attribute", function (string, string2) {
+  cy.get("#" + string).should('have.id', string).click();
+  }); 
